93731a2b4a14518894a0675f2ce233892c4a10f4
amy-verma <amitjsr8786@gmail.com>
2025-07-31 14:19:01 +0530

    features-of-react

--- useParams.js
+++ useParams.js
@@ -1,4 +1,5 @@
 What is useParams in React?
 
-The useParams hook is provided by React Router (usually from react-router-dom) and is used to access the dynamic parameters from the URL in a React component.
+The useParams hook is provided by React Router (usually from react-router-dom) and is used to access the dynamic 
+parameters from the URL in a React component.
 
