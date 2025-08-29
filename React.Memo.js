1ead8645118eef845c4e9a6e45642e5417a80848
amy-verma <amitjsr8786@gmail.com>
2025-08-29 17:42:18 +0530

    react.memo

--- React.Memo.js
+++ React.Memo.js
@@ -1,2 +1,5 @@
 // React.memo remembers the last result of a component and skips re-rendering it if
 //  the props haven't changed.
+
+// What it does:
+// Wraps a functional component to memoize its output. It will only re-render if props change.
